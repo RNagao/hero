abdec99135e2b427645637bc35ec9342eba8298e
rnagao <nagao1234@poli.ufrj.br>
2022-10-04 09:21:19 +0100

    Reading Keystrokes

--- src/main/java/Game.java
+++ src/main/java/Game.java
@@ -1,5 +1,7 @@
 import com.googlecode.lanterna.TerminalSize;
 import com.googlecode.lanterna.TextCharacter;
+import com.googlecode.lanterna.input.KeyStroke;
+import com.googlecode.lanterna.input.KeyType;
 import com.googlecode.lanterna.screen.Screen;
 import com.googlecode.lanterna.screen.TerminalScreen;
 import com.googlecode.lanterna.terminal.DefaultTerminalFactory;
@@ -9,6 +11,9 @@ import java.io.IOException;
 
 public class Game {
     private Screen screen;
+    private int x = 10;
+    private int y = 10;
+    Boolean play = Boolean.TRUE;
 
     public Game() {
         try {
@@ -29,11 +34,36 @@ public class Game {
 
     private void draw() throws IOException {
         screen.clear();
-        screen.setCharacter(10, 10, TextCharacter.fromCharacter('X')[0]);
+        screen.setCharacter(x, y, TextCharacter.fromCharacter('X')[0]);
         screen.refresh();
     }
 
     public void run() throws IOException {
-        draw();
+        while(play) {
+            draw();
+            KeyStroke key = screen.readInput();
+            processKey(key);
+        }
+    }
+
+    private void processKey(KeyStroke key) {
+        KeyType keyType = key.getKeyType();
+        if (keyType == KeyType.ArrowUp) {
+            y -= 1;
+        } else if (keyType == KeyType.ArrowDown) {
+            y += 1;
+        } else if (keyType == KeyType.ArrowLeft) {
+            x -= 1;
+        } else if (keyType == KeyType.ArrowRight) {
+            x += 1;
+        } else if (keyType == KeyType.Character && key.getCharacter() == 'q') {
+            try {
+                screen.close();
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        } else if (keyType == KeyType.EOF) {
+            play = Boolean.FALSE;
+        }
     }
 }
