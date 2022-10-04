1d3d5be3a05e7e8e66554ce63c739341956fc73a
rnagao <nagao1234@poli.ufrj.br>
2022-10-04 09:04:03 +0100

    Creating Game

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -9,7 +9,6 @@ import java.io.IOException;
 
 public class Application {
     public static void main(String[] args) {
-<<<<<<< HEAD
         Game game = new Game();
         try {
             game.run();
